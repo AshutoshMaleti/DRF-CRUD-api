6779fb8d9daadc8c6cffbbc91e0307b877831839
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-09-08 15:22:36 +0530

    removed obsolete imported modules

--- base/views.py
+++ base/views.py
@@ -1,6 +1,4 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
-from rest_framework import serializers
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
